b19a508dcc2597972f64686d09f99c0e4bb978e6
hybg1126 <hybg1126@gmail.com>
2025-09-02 17:36:21 +0800

    可用性验证

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,15 +6,15 @@ import java.text.ParseException;
 public class test {
 
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, ParseException {
 
 
-        HMAC dateFloor = new HMAC();
+        ShiftRightUnsigned dateFloor = new ShiftRightUnsigned();
         byte[] src = {'h','e','l','l','o',' ','w','o','r','l','d'};
         // 示例输入数据和密钥
         String data = "hello world";
         String key = "R/EF98s2xof7m16m+3b4n/0DDZbbTlDERdedJqfhP94=";
-        System.out.println(dateFloor.evaluate(data, key));
+        System.out.println(dateFloor.evaluate(4,1));
 
     }
 
